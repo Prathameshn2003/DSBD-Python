7fd9a3a5e0f43f7fa390be2ee93fc6d30697a22b
Prathameshn2003 <prathameshnalawade288@gmail.com>
2025-01-10 16:45:02 +0530

    Add files via upload

---
+++ set.py
@@ -0,0 +1,38 @@
+#set
+#set={1,2,3,2,"ram","world"}
+#print(set)#not allowed
+#print(type(set))
+#print(len(set))#not duplicate value allowcollection
+#collection=set()
+#collection.add(1)
+#collection.add(3)
+#collection.remove(3)#remove element
+#collection.pop()
+
+#print(collection)
+
+#union
+#set1={1,2,3,4}
+##set2={4,3,2,5}
+##print(set1.intersection(set2))
+#3print(set1.union(set2))
+
+#subject={"py","java","c","c++","c++","java"}
+#print(len(subject))
+
+#dictonary
+marks={}
+x=int(input("Enter phy:"))
+marks.update({"Phy":x})
+x=int(input("Enter math:"))
+marks.update({"math":x})
+x=int(input("Enter sci"))
+marks.update({"sci":x})
+x=int(input("Enter IOT"))
+marks.update({"IOT":x})
+print(marks)
+
+
+
+
+
