6d1f76c600dbabaf6777a7db3dce38cd80926656
Prathameshn2003 <prathameshnalawade288@gmail.com>
2025-01-10 15:08:08 +0530

    Add files via upload

---
+++ new.py
@@ -0,0 +1,8 @@
+#function
+str=input("Enter name")
+print(str.endswith("sh"))
+print(str.capitalize())
+str1=input("Enter your name")
+print(str.replace(str,str1))
+print(str.find("m"))
+print(str.count("a"))
