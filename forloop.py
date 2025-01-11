1194b33aee9a8dc198ff16aa06581fc666a0c902
Prathameshn2003 <prathameshnalawade288@gmail.com>
2025-01-11 16:15:41 +0530

    Add files via upload

---
+++ forloop.py
@@ -0,0 +1,166 @@
+#num=[1,4,3,6,7]
+#for i in num:
+ #   print(i)
+
+#str="prathamesh"
+#for i in str:
+ #    print(i)
+
+#list=[1,2,4,6,7]
+#for i in list:
+ #   print(i)
+
+#list=[1,4,2,5,6,7]
+#for i in list:
+ #   if(i%2==0):
+  #      print("even",i)
+   # else:
+    #    print("odd",i)
+
+#list=[1,4,5,6,7,8]
+#x=int(input("enter value"))
+
+#for i in list:
+ #   if(i==x):
+  #      print("found",i)
+
+#number=[3,4,5,1,7]
+#sum=0
+#for i in number:
+ #   sum=sum+i
+
+#print(sum)#print for barobarc ale pahije
+#sum=0
+#for i in range(1,10):
+ #   sum=sum+i
+
+#print(sum)
+
+
+#even=0
+#odd=0
+#for i in range(1,10):
+ #   if(i%2==0):
+  #      even=even+i
+
+   # else:
+    #    odd=odd+i
+#print("even sum",even)
+#print("odd sum",odd)
+
+#mulitiplication table
+#num=int(input("enter value"))
+#for i in range(1,11):
+ #   mul=num*i
+  #  print(mul)
+#Counting Vowels in a String
+#str="prathamesh"
+#vowel="aeiou"
+#count=0
+#for i in str:
+ #   if i in vowel:
+  #      count+=1
+#print(count)
+
+#Iterating Over a Dictionary
+#dict={"name":"prathamesh","age":20}
+#for key,value in dict.items():
+  #  print(key,value)
+#for i in dict:
+    #print(i,dict[i]) #key=i value=dict[i]
+
+#Finding Prime Numbers in a Range
+#for num in range(2, 20):
+ #   for i in range(2, num):
+  #      if num % i == 0:
+   #         break
+    #else:
+     #   print(f"{num} is a prime number")
+
+#Reversing a String
+#str="prathamesh"
+#rev=""
+#for i in str:
+ #   rev=i+rev
+#print(rev)
+
+#Finding the Maximum Number in a List
+
+#list=[1,2,3796,1,22,546,999,2124]
+#max=list[0]
+#for i in list:
+ #   if i>max:
+  #      max=i
+#print(max)
+
+#list=[73,34,12,667,566]
+#min=list[0]
+#for i in list:
+ #   if i<min:
+  #      min=i
+#print(min)
+
+#sentence = "This is an example sentence"
+#word_count = 0
+
+#for char in sentence:
+ #   if char == " ":
+  #    word_count += 1
+
+# Add 1 for the last word
+#word_count += 1
+
+#print(f"The sentence contains {word_count} words.")
+
+#continue for skip value
+#for i in range(1,10):
+ #   if i==4:
+  #     continue
+   # print(i)
+
+ #break
+#list=[2,3,4,5,6,8,9,4]
+#for i in list:
+ #   if i==5:
+  #      break
+   # print(i)
+
+#Creating a List of Squares
+#list=[2,3,4,1,6,7,4]
+#squre=[]
+#for i in list:
+ #   squre.append(i*i)
+#print(squre)
+
+
+#Finding the Length of Each Word in a List
+#list=["prathamesh","python","java","c"]
+#for i in list:
+  #  print(len(i))
+
+#list=[[1,2],[34,5]]
+#new_list=[]
+#for i in list:
+ #   for j in i:
+  #      new_list.append(j)
+#print(new_list)
+
+#Reversing a List
+#list=[1,2,5,7,4]
+#for i in reversed(list):
+ #   print(i)
+
+#Calculating the Factorial of a Number
+#fact=1
+#num=int(input("enter value"))
+#for i in range(1,num+1):
+ #   fact=fact*i
+#print(fact)
+
+#list=[23,4,5,1,6,7,8,4,46,66]
+#even=[]
+#for i in list:
+ #   if i%2==0:
+  #      even.append(i)
+#print(even)
+
